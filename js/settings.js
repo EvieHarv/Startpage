788024d54198b3710b8c4f0400fb8b3ec8bf6149
EthanHarv <github@ethanharv.com>
2021-06-25 00:42:31 -0500

    Handle link events

--- js/settings.js
+++ js/settings.js
@@ -1,3 +1,10 @@
+// I ALWAYS hit Ctrl+S even though I don't need to. This'll block the "save page" modal.
+document.onkeydown = KeyPress;
+function KeyPress(e) {
+    var evtobj = window.event? event : e;
+    if (evtobj.keyCode == 83 && evtobj.ctrlKey) { e.preventDefault(); };
+}
+
 // Open the main settings panel
 document.querySelector('.settings-button').addEventListener('click', function(e)
 {
@@ -29,7 +36,8 @@ function closeSettings() {
 // Transition to Links page
 document.querySelector('.settings-link-links').addEventListener('click', function(e)
 {
-    displayPanel('links', 750, 450);
+    const links = JSON.parse(localStorage.getItem('userLinks'));
+    displayPanel('links', 740, 60 + (44 * links.length));
 });
 
 // Transition to Weather page
@@ -63,18 +71,25 @@ function buildSettingsPanels()
     // Get all existing links
     const links = JSON.parse(localStorage.getItem('userLinks'));
     for (let i = 0; i < links.length; i++) {
-        const link = links.filter(obj => {
-            return obj.order === i;
-        })[0];
-        console.log(link)
+        const link = getLinkByOrder(links, i);
+        console.log(link);
 
+        // Bool, do we use an image?
         const displayImage = link['disp'] === "img" ? true : false;
 
+        // Container div
         var linkItem = document.createElement("div");
         linkItem.classList.add('link-item');
+        linkItem.dataset.order = link.order; // used for ident. purposes
+        var linkItemDelete = document.createElement("span");
+        linkItemDelete.classList.add('link-item-delete');
+        linkItemDelete.innerHTML = '&times;'; // ×
+        linkItem.appendChild(linkItemDelete);
 
-        var sortHandle = document.createElement("span");
+        // Handle
+        var sortHandle = document.createElement("a");
         sortHandle.classList.add('sorthandle');
+        sortHandle.href = '#';
         sortHandle.innerHTML = '≡';
         linkItem.appendChild(sortHandle);
 
@@ -89,8 +104,8 @@ function buildSettingsPanels()
         formLink.value = link.link;
         form.appendChild(formLink);
 
-        // Display type checkbox ("Use Image:")
-        form.append('Use Image:')
+        // Display type checkbox ("Use Image: [x]")
+        form.append(' Use Image:')
         var formCheckbox = document.createElement("input");
         formCheckbox.classList.add('checkbox'); formCheckbox.classList.add('form-display-type');
         formCheckbox.type = 'checkbox';
@@ -103,6 +118,7 @@ function buildSettingsPanels()
             formCheckbox.checked = false;
         };
         form.appendChild(formCheckbox);
+        form.append(' ')
         
         // Display URL/Text
         var formDisplay = document.createElement("input");
@@ -118,25 +134,26 @@ function buildSettingsPanels()
             formDisplay.placeholder = 'Text';
         };
         form.appendChild(formDisplay);
+
+        // Append items
         linkItem.appendChild(form);
         linksPanel.appendChild(linkItem);
     };
 
-    $('.form-display-type').change(function() {
-        if(this.checked) {
-            console.log(this);
-            $($(this).closest('form')[0]).find('.form-display')[0].placeholder = 'Image URL';
-        }
-        else {
-            $($(this).closest('form')[0]).find('.form-display')[0].placeholder = 'Text';
-        }
-    });
+    // Change placeholder text, depending on if it's a URL or Text input
+    $('.form-link').on('input', function(e) { linkHandleLinkChanged(e); });
+    $('.form-display-type').change(function(e) { linkHandleCheckboxCheck(e); });
+    $('.form-display').on('input', function(e) { linkHandleDisplayChanged(e); });
+    $('.link-item-delete').click(function(e) { linkHandleDeleteItem(e); });
 
+    // Set up as sortableJS items
     var el = document.querySelector('.settings-panel-links-items');
     var sortable = Sortable.create(el, {
         animation : 150,
-        handle: '.sorthandle'
+        handle: '.sorthandle',
+        onEnd : function(e){ linkHandleDrag(e); }
     });
+
     /////////////////////////
     //////// Weather ////////
     /////////////////////////
@@ -150,3 +167,147 @@ function buildSettingsPanels()
     const colorPanel = document.querySelector('.settings-panel-colors-items');
     colorPanel.innerHTML = '';
 };
+
+/////////////////////////////////
+///////// Handle Events /////////
+/////////////////////////////////
+
+/////////////////////////
+///////// Links /////////
+/////////////////////////
+
+function linkHandleCheckboxCheck(e)
+{
+    const target = e.target;
+    const linkItem = $(target).closest('.link-item')[0];
+    const links = JSON.parse(localStorage.getItem('userLinks'));
+    const order = linkItem.dataset.order;
+    const index = getIndexByOrder(links, order);
+    
+    if(target.checked) {
+        $(linkItem).find('.form-display')[0].placeholder = 'Image URL';
+        links[index].disp = "img";
+    }
+    else {
+        $(linkItem).find('.form-display')[0].placeholder = 'Text';
+        links[index].disp = "text";
+    }
+
+    localStorage.setItem('userLinks', JSON.stringify(links));
+    buildCircles();
+};
+
+function linkHandleLinkChanged(e)
+{
+    const target = e.target;
+    const linkItem = $(target).closest('.link-item')[0];
+    const links = JSON.parse(localStorage.getItem('userLinks'));
+    const order = linkItem.dataset.order;
+    const index = getIndexByOrder(links, order);
+    
+    links[index].link = target.value;
+
+    localStorage.setItem('userLinks', JSON.stringify(links));
+    buildCircles();
+}
+
+function linkHandleDisplayChanged(e)
+{
+    const target = e.target;
+    const linkItem = $(target).closest('.link-item')[0];
+    const links = JSON.parse(localStorage.getItem('userLinks'));
+    const order = linkItem.dataset.order;
+    const index = getIndexByOrder(links, order);
+    
+    links[index].dispText = target.value;
+
+    localStorage.setItem('userLinks', JSON.stringify(links));
+    buildCircles();
+}
+
+function linkHandleDeleteItem(e)
+{
+    const target = e.target;
+    const linkItem = $(target).closest('.link-item')[0];
+
+    linkItem.remove();
+    
+    const links = buildArrayFromPage();
+    
+    displayPanel('links', 740, 60 + (44 * links.length));
+    
+    localStorage.setItem('userLinks', JSON.stringify(links));
+    buildCircles();
+}
+
+
+$('#addNewLink').click(function(e){
+    linkHandleAddItem(e);
+});
+
+function linkHandleAddItem(e)
+{
+    // Get current
+    var links = buildArrayFromPage();
+    // Add
+    links.push(
+    {
+        order: links.length,
+        link: '',
+        disp: 'text',
+        dispText: ''
+    });
+
+    // Widen panel
+    displayPanel('links', 740, 60 + (44 * links.length));
+
+    // Assign and build
+    localStorage.setItem('userLinks', JSON.stringify(links));
+    buildCircles();
+    buildSettingsPanels();
+}
+
+function linkHandleDrag(e)
+{    
+    const links = buildArrayFromPage();
+
+    localStorage.setItem('userLinks', JSON.stringify(links));
+    buildCircles();
+}
+
+
+
+/// Helper Functions ///
+
+function buildArrayFromPage()
+{
+    const links = [];
+    $('.settings-panel-links-items').children().each(function(index, item){
+        const order = index;
+        const link = $(item).find('.form-link').val();
+        const disp = $(item).find('.form-display-type')[0].checked ? "img" : "text";
+        const dispText = $(item).find('.form-display').val();
+        links.push({
+            order : order,
+            link : link,
+            disp: disp,
+            dispText : dispText
+        });
+    });
+    return links;
+}
+
+
+function getLinkByOrder(links, order)
+{
+    const link = links.filter(obj => {
+        return obj.order == order;
+    })[0];
+    return link;
+}
+
+function getIndexByOrder(links, order)
+{
+    const index = links.indexOf(getLinkByOrder(links, order));
+    return index;
+}
